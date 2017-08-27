9407ea0d87e78b4efff8f4abe2f3894536302f82
Julia Kim <jkim0620@gmail.com>
2017-08-26 21:19:16 -0400

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,13 @@
+// Import a librabry to help create a component
+import React from 'react';
+import ReactNative from 'react-native';
+
+// Create a component
+const App = () => {
+  return (
+      <Text>Some Text</Text>
+  );
+};
+
+// Render it to the device
+ReactNative.AppRegistry.registerComponent('albums', () => App); 
