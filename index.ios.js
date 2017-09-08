fd888b2ccd0b15f04af690d73041a75375fac1f1
Julia Kim <jkim0620@gmail.com>
2017-09-08 15:39:29 -0400

    Refactor components

--- index.ios.js
+++ index.ios.js
@@ -2,13 +2,17 @@
 
 // Import a librabry to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => {
   return (
-    <Header />
+    <View>
+      <Header headerText={'Albums'}/>
+      <AlbumList />
+    </View>
   );
 };
 
