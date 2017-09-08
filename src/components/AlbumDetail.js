2377512bfb75d06b8f524a1cf132c1a722357378
Julia Kim <jkim0620@gmail.com>
2017-09-08 16:41:12 -0400

    Add styles to Card sections

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,13 +1,66 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    thumbnailStyle,
+    headerContentStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    albumImageStyle
+  } = styles;
+
   return (
     <Card>
-      <Text>{props.album.title}</Text>
+      <CardSection>
+        <View style={thumbnailContainerStyle}>
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+
+      <CardSection>
+        <Image
+          style={albumImageStyle}
+          source={{ uri: image }}
+        />
+      </CardSection>
     </Card>
   );
 };
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+  headerTextStyle: {
+    fontSize: 18,
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    marginLeft: 10,
+    marginRight: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  albumImageStyle: {
+    flex: 1,
+    width: null,
+    height: 300,
+  }
+}
 
 export default AlbumDetail;
